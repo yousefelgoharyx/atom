2f9d4a44677397879c73913702dad893e164cce1
Yousef <yousefelgoharyx@gmail.com>
2023-03-01 13:07:08 +0200

    upgrade std

--- src/packages/cli/deps.ts
+++ src/packages/cli/deps.ts
@@ -1,5 +1,5 @@
-export { Untar } from "https://deno.land/std@0.170.0/archive/untar.ts";
-export * as Colors from "https://deno.land/std@0.170.0/fmt/colors.ts";
-export { copy } from "https://deno.land/std@0.170.0/streams/copy.ts";
-export { readerFromStreamReader } from "https://deno.land/std@0.170.0/streams/reader_from_stream_reader.ts";
-export { ensureDir } from "https://deno.land/std@0.170.0/fs/ensure_dir.ts";
+export { Untar } from "https://deno.land/std@0.177.0/archive/untar.ts";
+export * as Colors from "https://deno.land/std@0.177.0/fmt/colors.ts";
+export { copy } from "https://deno.land/std@0.177.0/streams/copy.ts";
+export { readerFromStreamReader } from "https://deno.land/std@0.177.0/streams/reader_from_stream_reader.ts";
+export { ensureDir } from "https://deno.land/std@0.177.0/fs/ensure_dir.ts";
