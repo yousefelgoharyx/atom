f5c685f356d04428132c45a201f50bd5e8bdcb1d
Yousef <yousefelgoharyx@gmail.com>
2023-03-09 10:53:08 +0200

    nested routing - watch routes - plugin system

--- example/main.ts
+++ example/main.ts
@@ -1,9 +1,24 @@
-import Atom from "../mod.ts";
+import Atom, { PluginConfig } from "../mod.ts";
 
+function TimeLogger(): PluginConfig {
+  let start: number;
+  let url: string;
+  return {
+    interceptRequest: (req) => {
+      url = new URL(req.url).pathname;
+      start = Date.now();
+    },
+    interceptResponse: (res) => {
+      console.log(`${url} Finished in ${Date.now() - start}`);
+    },
+  };
+}
 async function main() {
+  Atom.install(TimeLogger);
+
   await Atom.bootstrap({
-    routesDir: "example/routes",
-    publicDir: "example/public",
+    routesDir: "example/src/routes",
+    publicDir: "example/src/public",
   });
 }
 
